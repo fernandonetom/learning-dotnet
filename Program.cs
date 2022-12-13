08f0c915390023b6ec81cbf69f791a84f98222a1
Fernando <bliteti.fernandoneto@unidas.com.br>
2022-12-13 17:16:30 -0300

    feat: json response

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/user", () => new { name = "test", age = 25 });
+app.MapGet("/addheader", (HttpResponse response) => response.Headers.Add("Test", "Content"));
 
 app.Run();
