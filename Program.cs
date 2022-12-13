2278095bf0b9ef1c97eaf0bfad7fb485346d99be
Fernando <bliteti.fernandoneto@unidas.com.br>
2022-12-13 17:30:17 -0300

    feat: params from query and route

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,4 +7,27 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/user", () => new { name = "test", age = 25 });
 app.MapGet("/addheader", (HttpResponse response) => response.Headers.Add("Test", "Content"));
 
+
+app.MapPost("/saveproduct", (Product product) =>
+{
+  return $"{product.Code} - {product.Name}";
+});
+
+app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string? dateEnd) =>
+{
+  return dateStart;
+});
+
+app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
+{
+  return code;
+});
+
 app.Run();
+
+
+public class Product
+{
+  public string? Code { get; set; }
+  public string? Name { get; set; }
+}
